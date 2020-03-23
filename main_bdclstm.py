d794ab231a0a838b00a675cd045daf87da2d727e
vpamrit <vishnuamritpydah@gmail.com>
2020-03-22 22:04:41 -0400

    -

--- main_bdclstm.py
+++ main_bdclstm.py
@@ -59,7 +59,7 @@ CLASSES = [1,6,7,8,9,11]
 
 # %% Loading in the Dataset
 slice_size = 240
-dset_train = SpleenDatasetBuilder(DATA_FOLDER, (1, 1)).dataset
+dset_train = SpleenDatasetBuilder(DATA_FOLDER, (1, 4)).dataset
 dset_valid = SpleenDatasetBuilder(DATA_FOLDER, (0, 0)).dataset
 
 train_loader = DataLoader(dset_train, batch_size=args.batch_size, num_workers=1)
