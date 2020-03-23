a80cb44fb8ace0b523ba8f17ad032bc485a7b2a8
vpamrit <vishnuamritpydah@gmail.com>
2020-03-22 22:12:17 -0400

    -

--- load_data.py
+++ load_data.py
@@ -89,7 +89,7 @@ class SpleenDatasetBuilder:
             samples_per_volume=10,
             sampler_class=torchio.data.GridSampler,
             patch_size=(3, 240, 240),
-            num_workers=1,
+            num_workers=4,
             shuffle_subjects=False,
             shuffle_patches=True
         )
